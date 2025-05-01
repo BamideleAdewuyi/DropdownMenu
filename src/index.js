6cb9c5ffa482b73914afa31844b6ddd908d98d52
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-05-01 17:58:54 +0100

    index.js: Created dropdownMenuOption1 const

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./style.css";
 
 const button1 = document.getElementById("menuToggleButton1");
 const menu1 = document.getElementById("dropdownMenu1");
+const dropdownMenuOption1 = document.querySelectorAll(".dropdownMenuOption1")
 
 class Visible {
     constructor(menuToggleButton, dropdownMenu) {
