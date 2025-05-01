d51819998d3e50344a1d4f1f4046bc6a6f53e711
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-05-01 14:57:27 +0100

    index.js: Called addListener method in Visible class constructor. Can now view options by clicking menuToggleButton

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ class Visible {
     constructor() {
         this.menuToggleButton = document.getElementById("menuToggleButton")
         this.dropdownMenu = document.querySelector(".dropdownMenu");
+        this.addListener();
     }
 
     addListener() {
