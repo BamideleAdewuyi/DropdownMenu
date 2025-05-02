95096ce6fb30a5237e4c5e2042b6ede589ce07f1
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-05-02 14:33:53 +0100

    index.js: Set up second dropdown menu using another instance of Visible class

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const button1 = document.getElementById("menuToggleButton1");
 const menu1 = document.getElementById("dropdownMenu1");
 const dropdownMenuOptions1 = document.querySelectorAll(".dropdownMenuOptions1")
 
+const button2 = document.getElementById("menuToggleButton2")
+const menu2 = document.getElementById("dropdownMenu2");
+const dropdownMenuOptions2 = document.querySelectorAll(".dropdownMenuOptions2")
+
 class Visible {
     constructor(menuToggleButton, dropdownMenu, dropdownMenuOptions) {
         this.menuToggleButton = menuToggleButton;
@@ -33,3 +37,4 @@ class Visible {
 };
 
 const visible = new Visible(button1, menu1, dropdownMenuOptions1);
+const visible2 = new Visible(button2, menu2, dropdownMenuOptions2);
