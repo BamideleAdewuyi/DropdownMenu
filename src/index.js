0eeec4d597a226077e332933bed2226162126b5b
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-05-02 15:43:37 +0100

    index.js: Imported Visible class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import "./style.css";
+import "./style.css"
+import Visible from "./visible";
 
 const button1 = document.getElementById("menuToggleButton1");
 const menu1 = document.getElementById("dropdownMenu1");
@@ -7,43 +8,6 @@ const dropdownMenuOptions1 = document.querySelectorAll(".dropdownMenuOptions1")
 const button2 = document.getElementById("menuToggleButton2")
 const menu2 = document.getElementById("dropdownMenu2");
 const dropdownMenuOptions2 = document.querySelectorAll(".dropdownMenuOptions2")
-
-class Visible {
-    constructor(menuToggleButton, dropdownMenu, dropdownMenuOptions) {
-        this.menuToggleButton = menuToggleButton;
-        this.dropdownMenu = dropdownMenu;
-        this.dropdownMenuOptions = dropdownMenuOptions;
-        this.menuHeight = this.getHeight();
-        this.addListeners();
-        this.hideMenu();
-    }
-
-    addListeners() {
-        this.menuToggleButton.addEventListener("click", () => this.showMenu())
-
-        this.dropdownMenuOptions.forEach(option => {
-            option.addEventListener("click", () => {
-                this.hideMenu();
-                this.menuToggleButton.textContent = option.textContent
-            })
-        });
-    }
-
-    showMenu() {
-        this.dropdownMenu.style.visibility = "visible";
-        this.dropdownMenu.style.height = `${this.menuHeight}px`;
-    }
-
-    hideMenu() {
-        this.dropdownMenu.style.visibility = "hidden";
-        this.dropdownMenu.style.height = "0";
-    }
-
-    getHeight() {
-        const menuHeight = this.dropdownMenu.offsetHeight;
-        return menuHeight;
-    }
-};
 
 const visible = new Visible(button1, menu1, dropdownMenuOptions1);
 const visible2 = new Visible(button2, menu2, dropdownMenuOptions2);
